24f7baf85cceecbad278b12668e81bf64cba8688
Alexandre GAULT <alexandre.gault@insa-cvl.fr>
2018-07-09 15:54:39 +0200

    added length of the path

--- projet_astar.py
+++ projet_astar.py
@@ -2,8 +2,6 @@ from tkinter import *
 import random
 import time
 
-root = Tk()
-
 TIME = 0.05
 
 CELL_SIZE = 40
@@ -83,7 +81,7 @@ def create_path():
     while pos2 == pos1 or pos2 in OBSTACLES:
         pos2 = (random.randint(0,15), random.randint(0,15))
 
-    print("path between ", pos1, pos2)
+    print("path between ", pos1, pos2, "length : ", end = "")
     x1, y1 = pos1
     x2, y2 = pos2
 
@@ -93,28 +91,30 @@ def create_path():
     time.sleep(TIME)
     path = getPath(pos1, pos2)
     if path != -1:
+        print(len(path))
         for cell in path:
             x, y = cell
             grid.create_rectangle(x*CELL_SIZE, y*CELL_SIZE, (x+1)*CELL_SIZE, (y+1)*CELL_SIZE, fill='blue')
     else:
         print("NO PATH")
     grid.create_rectangle(x2*CELL_SIZE, y2*CELL_SIZE, (x2+1)*CELL_SIZE, (y2+1)*CELL_SIZE, fill='red')
-    
-    startButton.config(state = NORMAL)
+    startButton.config(state=NORMAL)
 
-grid = Canvas(root, width = 16*CELL_SIZE, height = 16*CELL_SIZE, bg = 'white')
-grid.pack()
-OBSTACLES = []
-for x in range(16):
-    grid.create_line(0, x*CELL_SIZE, 16*CELL_SIZE, x*CELL_SIZE)
-    grid.create_line(x*CELL_SIZE, 0, x*CELL_SIZE, 16*CELL_SIZE)
+if __name__ == "__main__":
 
-for i in range(NUMBER_OF_OBSTACLES):
-    x, y = random.randint(0, 15), random.randint(0, 15)
-    OBSTACLES.append((x, y))
-    grid.create_rectangle(x*CELL_SIZE, y*CELL_SIZE, (x+1)*CELL_SIZE, (y+1)*CELL_SIZE, fill='black')
-    
-startButton = Button(root, text = 'start', command = create_path)
-startButton.pack()
+    root = Tk()
+    grid = Canvas(root, width = 16*CELL_SIZE, height = 16*CELL_SIZE, bg = 'white')
+    grid.pack()
+    OBSTACLES = []
+    for x in range(16):
+        grid.create_line(0, x*CELL_SIZE, 16*CELL_SIZE, x*CELL_SIZE)
+        grid.create_line(x*CELL_SIZE, 0, x*CELL_SIZE, 16*CELL_SIZE)
 
-root.mainloop()
+    for i in range(NUMBER_OF_OBSTACLES):
+        x, y = random.randint(0, 15), random.randint(0, 15)
+        OBSTACLES.append((x, y))
+        grid.create_rectangle(x*CELL_SIZE, y*CELL_SIZE, (x+1)*CELL_SIZE, (y+1)*CELL_SIZE, fill='black')
+        
+    startButton = Button(root, text = 'start', command = create_path)
+    startButton.pack()
+    root.mainloop()
