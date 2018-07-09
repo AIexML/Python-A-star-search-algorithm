e742bdf8b94f5c50cb9a7531722b41801db0b3d9
Alexandre GAULT <alexandre.gault@insa-cvl.fr>
2018-07-09 15:42:05 +0200

    variable position swapped from list to tuple

--- projet_astar.py
+++ projet_astar.py
@@ -10,9 +10,9 @@ CELL_SIZE = 40
 
 NUMBER_OF_OBSTACLES = 90
 
-def getDistance(pos, pos2):
+def getDistance(pos1, pos2):
     """Returns the Manhattan distance between pos1 and pos2"""
-    return abs(pos[0]-pos2[0])+abs(pos[1]-pos2[1])
+    return abs(pos1[0]-pos2[0])+abs(pos1[1]-pos2[1])
 
 def getPath(start, end):
     """Returns a path between start and end, if it exists"""
@@ -20,14 +20,13 @@ def getPath(start, end):
     if start == end:
         return -1
     openList = [start]
-    nodes = {str(start) : ['',0]} # nodes['[x, y]'] = [parent, distance from the start]
+    nodes = {start : ['',0]} # nodes['[x, y]'] = [parent, distance from the start]
     closedList = []
     x1, y1 = start
     while openList:
-        
         current = openList[0]
         for tmp in openList:
-            if nodes[str(tmp)][1] + getDistance(tmp, end) < nodes[str(current)][1] + getDistance(current, end):
+            if nodes[tmp][1] + getDistance(tmp, end) < nodes[current][1] + getDistance(current, end):
                 current = tmp
         x, y = current
         grid.create_rectangle(x*CELL_SIZE, y*CELL_SIZE, (x+1)*CELL_SIZE, (y+1)*CELL_SIZE, fill='yellow')
@@ -38,16 +37,16 @@ def getPath(start, end):
         del openList[openList.index(current)]
         closedList.append(current)
         
-        for a,b in [[0,1], [0,-1], [1,0], [-1,0]]:
+        for a,b in [(0,1), (0,-1), (1,0), (-1,0)]:
             X = current[0] + a
             Y = current[1] + b
-            if [X, Y] in OBSTACLES or [X,Y] in closedList or X < 0 or X > 15 or Y < 0 or Y > 15:
+            if (X, Y) in OBSTACLES or (X,Y) in closedList or X < 0 or X > 15 or Y < 0 or Y > 15:
                 continue
-            elif not [X,Y] in openList:
-                openList.append([X,Y])
-                nodes[str([X,Y])] = [current, nodes[str(current)][1] + 1]
-            elif not str([X,Y]) in nodes or nodes[str([X,Y])][1] > nodes[str(current)][1] + 1:
-                nodes[str([X,Y])] = [current, nodes[str(current)][1] + 1]
+            elif not (X,Y) in openList:
+                openList.append((X,Y))
+                nodes[(X,Y)] = [current, nodes[current][1] + 1]
+            elif not (X,Y) in nodes or nodes[(X,Y)][1] > nodes[current][1] + 1:
+                nodes[(X,Y)] = [current, nodes[current][1] + 1]
         root.update()
         
         time.sleep(TIME)
@@ -56,13 +55,12 @@ def getPath(start, end):
     if current != end: # if the path does not exist
         return -1
 
-    tmp = nodes[str(end)][0]
+    tmp = nodes[end][0]
     path = [end]
     while tmp != start: # rewind the parents of the nodes to get the path
         path.append(tmp)
-        tmp = nodes[str(tmp)][0]
+        tmp = nodes[tmp][0]
     return path[::-1]
-
 
 def refreshMap():
     global CELL_SIZE
@@ -78,12 +76,12 @@ def create_path():
     startButton.config(state = DISABLED)
     global CELL_SIZE
     refreshMap()
-    pos1 = [random.randint(0,15), random.randint(0,15)]
+    pos1 = (random.randint(0,15), random.randint(0,15))
     while pos1 in OBSTACLES:
-        pos1 = [random.randint(0,15), random.randint(0,15)]
+        pos1 = (random.randint(0,15), random.randint(0,15))
     pos2 = pos1
     while pos2 == pos1 or pos2 in OBSTACLES:
-        pos2 = [random.randint(0,15), random.randint(0,15)]
+        pos2 = (random.randint(0,15), random.randint(0,15))
 
     print("path between ", pos1, pos2)
     x1, y1 = pos1
@@ -113,7 +111,7 @@ for x in range(16):
 
 for i in range(NUMBER_OF_OBSTACLES):
     x, y = random.randint(0, 15), random.randint(0, 15)
-    OBSTACLES.append([x, y])
+    OBSTACLES.append((x, y))
     grid.create_rectangle(x*CELL_SIZE, y*CELL_SIZE, (x+1)*CELL_SIZE, (y+1)*CELL_SIZE, fill='black')
     
 startButton = Button(root, text = 'start', command = create_path)
