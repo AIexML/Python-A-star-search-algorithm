98ffb74936b88454305ae8c7b1b831455554c607
Alexandre GAULT <alexandre.gault@insa-cvl.fr>
2018-07-12 11:01:26 +0200

    variable renamed

--- astar.py
+++ astar.py
@@ -12,34 +12,35 @@ def get_path(start, end, OBSTACLES, SIZE):
     closedList = []
     x1, y1 = start
     while openList:
-        current = openList[0]
-        for tmp in openList:
-            if nodes[tmp][1] + get_distance(tmp, end) < nodes[current][1] + get_distance(current, end):
-                current = tmp
-        x, y = current
-        if current == end:
+        current_node = openList[0]
+        for tmp in openList[1:]:
+            if nodes[tmp][1] + get_distance(tmp, end) < nodes[current_node][1] + get_distance(current_node, end):
+                current_node = tmp
+        x, y = current_node
+        if current_node == end:
             break
 
-        openList.remove(current)
-        closedList.append(current)
+        openList.remove(current_node)
+        closedList.append(current_node)
         
         for a, b in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-            X = current[0] + a
-            Y = current[1] + b
-            if (X, Y) in OBSTACLES or (X, Y) in closedList or X < 0 or X >= SIZE or Y < 0 or Y >= SIZE:
+            X = current_node[0] + a
+            Y = current_node[1] + b
+            new_node = (X, Y)
+            if new_node in OBSTACLES or new_node in closedList or X < 0 or X >= SIZE or Y < 0 or Y >= SIZE:
                 continue
-            elif not (X, Y) in openList:
-                openList.append((X, Y))
-                nodes[(X, Y)] = [current, nodes[current][1] + 1]
-            elif not (X, Y) in nodes or nodes[(X, Y)][1] > nodes[current][1] + 1:
-                nodes[(X, Y)] = [current, nodes[current][1] + 1]
+            elif not new_node in openList:
+                openList.append(new_node)
+                nodes[new_node] = [current_node, nodes[current_node][1] + 1]
+            elif not new_node in nodes or nodes[new_node][1] > nodes[current_node][1] + 1:
+                nodes[new_node] = [current_node, nodes[current_node][1] + 1]
         
-    if current != end:  # if the path does not exist
+    if current_node != end:  # if the path does not exist
         return None, closedList
 
-    tmp = nodes[end][0]
+    parent = nodes[end][0]
     path = [end]
-    while tmp != start:  # rewind the parents of the nodes to get the path
-        path = [tmp] + path
-        tmp = nodes[tmp][0]
+    while parent != start:  # rewind the parents of the nodes to get the path
+        path = [parent] + path
+        parent = nodes[parent][0]
     return path, closedList
